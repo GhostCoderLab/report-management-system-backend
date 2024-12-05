dc6f1ef75cab555fbeedd2d256ae45b8bcc1b191
GhostCoderLab <foundation.ares@gmail.com>
2024-12-05 14:22:09 +0000

    refactor: models.py

--- api/models.py
+++ api/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-# コマンド：python manage.py runserver --settings config.settings.development
-# コマンド：python manage.py makemigrations ares --settings config.settings.development
-# コマンド：python manage.py migrate --settings config.settings.development
-# SQLコマンド：DROP DATABASE app;
-# SQLコマンド：CREATE DATABASE app;
-# SQLコマンド：SELECT * FROM app.Report;
-
-# SQLコマンド：USE app;INSERT INTO Report (name,level,description) VALUES('破損報告書','レベル1','壊しました。');
-
 class Report(models.Model):
     name = models.CharField(max_length=200)
     level = models.CharField(max_length=50)
